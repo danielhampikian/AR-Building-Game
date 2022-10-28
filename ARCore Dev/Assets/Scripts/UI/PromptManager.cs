455219cbc081217c26ebf33c34760ef812f0c578
Daniel Hampikian <danielhampikian@gmail.com>
2022-10-27 19:09:20 -0600

    Update PromptManager.cs

--- ARCore Dev/Assets/Scripts/UI/PromptManager.cs
+++ ARCore Dev/Assets/Scripts/UI/PromptManager.cs
@@ -4,8 +4,100 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
+/*
+The main comment I have on this code as far as improving the archetecture is
+To make a seperate class for Prompts that hold the narrative, the choices, and the
+consequences for each meter.  This way you can auto generate prompts that requuire only 
+a slight change in the wording to give the player more flexibilty and choice, and 
+when future developers wnt to add to the game, they only have to fill out the 
+prototype for a prompt class so the code will be easier to maintain and add to over time
+
+Here's what I mean:
+
+//First have a class that holds static getters and setters to the resources so the other classes 
+//are forced to change data in a way that makes sense to update if you add another resource in 
+// the future you can do so flexibly and easily and we force the intialization to hold both a name that won't be changed once intialize and a value amount that 
+//
+
+
+public static class Resource{
+
+    public static string ResourceName;
+    public static int ResourceAmt {get; set;} 
+
+    public Resource(string resourceName, int resourceValue) {
+        ResourceName = resourceName;
+        ResourceAmt = resourceValue;
+    }
+}
+
+public class Resources {
+    public Resources resourcesSingleton;
+    public int numOfResources;
+    public int [] initialResourceVal;
+    public string[] resourceNames;
+    public static Resource[] resources {get; set;}
+    public static Resources() {
+            if (resourcesSingleton != null) {
+                resorucesSingletone = null;
+            }
+            resourcesSingleton = this;
+
+            //now we have a single instance of all our resource values
+            //accessable from anywhere in our classes with getter sna setters
+
+            //we initialize like this if we had one rosource, if we had more we can just add them to the array in teh editor
+            numOfResources = 1; //for example if we only had one, this can change
+            //assue the int and string array ar intiailzed to reflect this
+            resources = new Resource[numOfResources];
+            for (int i = 0; i < resource.Count; i ++) {
+                resoruces[i].set(new Resource("Happiness" //which would be resourceNames[0] 
+                ,0 //which would be intialResourceVal[0]
+                //and so on for the full array of names and values which can now be set in the editor
+                 ));
+            }
+    }
+}
+public class Prompt {
+
+    public string promptText;
+    public int numOfPossibleChoices;
+    public Dictionary choiceAndConsequenceDict;
+    public Dictionary consuenceTextAndResourceValues;
+
+    public Prompt(string promptT, Dictionary choicCons<string s, Dictionary consToResource<string text, int[] resourceChangeValues>>) {
+        //now when you make a prompt you create a dictionary of the choice to the choice textual consequemce, 
+        // and use that text as a key for the second dictionary which will be from the text conseuquence to the 
+        // int amount for each resource that you will change.
+
+        //I think that setting up the data structues liek this so that you change the reosurces 
+        //as a single peice of code that is accessed via the resourceSingleon with a structure like this:
+        /*
+            {choice text (the first key in the dictionary): 
+                consequence of choice text (the first vale and the second key to the
+                second dictionary that leads to the numerical changes) : 
+                    int[] value changes to resources (the final value of the second layer of the dictionary) that would look like [1-,0,-10,0] for four resources 
+            }
+
+            //if you have any questions about how to implement let me know, I'l comment out the classes above so it doesn't break your code
+
+    }
+
+
+/*The advantage of doing it this way is not only does the structure of the code garuntee flexibility and standardzied ways of accessing
+the resources and choices rather than indexing parrallel lists which is fragile for a larger game, but that now we can do ethings like put
+variable string to string to numerical value conseuqences in the prompt class so that if we change the term "noun that is the thing we are to invest in or not invest in"
+and the parrallel int [] values to draw from a randomized list, then we have actual variation in the path's the game can take that are potentially infite and repeating only 
+after long gameplay times noticable to the player.  Invest in staduim or library becomes then invest in a randomized choice of ----> 
+[array of words that you might invest in and coressponding int array of consewuences these terms have on resources]
+    
+}
+
+*/
 
 public class PromptManager : MonoBehaviour
+
+
 
 
 {
